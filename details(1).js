4bdb14523820735436762b39d4bbe6a3d3b81678
WE4EVERKOMBAN <108579654+WE4EVERKOMBAN@users.noreply.github.com>
2022-07-04 16:09:04 +0530

    Update details(1).js

--- details(1).js
+++ details(1).js
@@ -1,6 +1,6 @@
 module.exports = {
-    token: '5166228737:AAGNGYOOX6chNLMA9KMFvnR-zZq_GYq470E',
-    admins: [5035811352],
+    token: '5452754467:AAGn3rnqJoEtwFsXcegBtcYdgsPhVhLmmqI',
+    admins: [895687774],
     curr:'INR'
 }
 
